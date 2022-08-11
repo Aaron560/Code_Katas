ccd54a9098533518c366dc53439f2c9d171a8dc6
Aaron560 <aaron.ashcraft7@gmail.com>
2022-08-10 22:04:15 -0400

    todays solved Problems.

---
+++ Solutions/Ruby/6Kyu/Uniq-in-order.rb
@@ -0,0 +1,7 @@
+def unique_in_order(iterable)
+  #your code here
+  result = []
+  iterable = iterable.split("") if !iterable.is_a? Array
+  iterable.each_with_index { |x, i| result << x if iterable[i - 1] != x || i == 0 }
+  result
+end
